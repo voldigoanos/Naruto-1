2bcc978e110a3d4298b46c0eaf6d65a00f833401
1345-jr <drgok9556@gmail.com>
2024-10-29 11:46:12 +0100

    Create ia.js

---
+++ scripts/cmds/ia.js
@@ -0,0 +1,97 @@
+const axios = require('axios');
+
+async function fetchFromAI(url, params) {
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
+}
+
+async function getAIResponse(input, userId, messageID) {
+ const services = [
+ { url: 'https://ai-tools.replit.app/gpt', params: { prompt: input, uid: userId } },
+ { url: 'https://openaikey-x20f.onrender.com/api', params: { prompt: input } },
+ { url: 'http://fi1.bot-hosting.net:6518/gpt', params: { query: input } },
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
+
+ let response = ".... Désolé mais je ne sais pas....";
+ let currentIndex = 0;
+
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Move to the next service in the cycle
+ }
+
+ return { response, messageID };
+}
+
+module.exports = {
+ config: {
+ name: 'ia',
+ author: 'Arn',
+ role: 0,
+ category: 'AI',
+ shortDescription: 'ai to ask anything',
+ },
+ onStart: async function ({ api, event, arns }) {
+          const helpListImages = [
+        "https://tinyurl.com/27lldnwf", // add image link here
+        "https://tinyurl.com/2yxokypt",
+        "https://tinyurl.com/2chpu8hn",
+        "https://tinyurl.com/2d76p34a",
+        "https://tinyurl.com/2dmdw9qh",
+        // Add more image links as needed
+      ];
+
+      const helpListImage = helpListImages[Math.floor(Math.random() * helpListImages.length)];
+
+      await message.reply({
+        body: msg,
+        attachment: await global.utils.getStreamFromURL(helpListImage),
+      });
+ const input = args.join(' ').trim();
+ if (!input) {
+ api.sendMessage(``, event.threadID, event.messageID);
+ return;
+ }
+
+const fonts = {
+
+ mathsans: {
+
+ a: "𝑎", b: "𝑏", c: "𝑐", d: "𝑑", e: "𝑒", f: "𝑓", g: "𝑔", h: "ℎ", i: "𝑖",
+
+ j: "𝑗", k: "𝑘", l: "𝑙", m: "𝑚", n: "𝑛", o: "𝑜", p: "𝑝", q: "𝑞", r: "𝑟",
+
+ s: "𝑠", t: "𝑡", u: "𝑢", v: "𝑣", w: "𝑤", x: "𝑥", y: "𝑦", z: "𝑧",
+
+ A: "𝐴", B: "𝐵", C: "𝐶", D: "𝐷", E: "𝐸", F: "𝐹", G: "𝐺", H: "𝐻", I: "𝐼",
+
+ J: "𝐽", K: "𝐾", L: "𝐿", M: "𝑀", N: "𝑁", O: "𝑂", P: "𝑃", Q: "𝑄", R: "𝑅",
+
+ S: "𝑆", T: "𝑇", U: "𝑈", V: "𝑉", W: "𝑊", X: "𝑋", Y: "𝑌", Z: "𝑍",
+ }
+};
+
+ 
+ const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
+ api.sendMessage(` ${response} `, event.threadID, messageID);
+ },
+ onChat: async function ({ event, message }) {
+ const messageContent = event.body.trim().toLowerCase();
+ if (messageContent.startsWith("ia")) {
+ const input = messageContent.replace(/^ia\*/, "").trim();
+ const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
+ message.reply( `𝙄𝙉𝙏𝙀𝙇𝙇𝙄𝙂𝙀𝙉𝘾𝙀 𝘼𝙍𝙏𝙄𝙁𝙄𝘾𝙄𝙀𝙇𝙇𝙀\n━━━━━━━━━━━━━━━━\n${response} ﹏✏️`, messageID);
+ }
+ }
+  }
